36d11ee01b79726236cfb00ccc335275d6cc35fd
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-06-22 18:22:31 -0300

    Criado metodo delete, otimizado arquivo app, definida as rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import db from "./config/dbConnect.js"
-import livros from './models/Livro.js';
 import routes from './routes/index.js'
 
 db.on("error", console.log.bind(console,'Erro de conexão'))
@@ -9,36 +8,7 @@ db.once("open", () => {
 })
 
 const app = express(); //iniciando o express
-
 app.use(express.json()) //interpreta o que chega via post e via put, transformando em obj e armazenando
-
-routes(app);
-
-//const livros = [
-//  {id: 1, 'titulo': 'Senhor dos aneis'},
-//  {id: 2, 'titulo': 'O Hobbit'}]
-
-app.get('/livros/:id',(req, res) => {
-  let index = buscarLivro(req.params.id); //utiliza a funcao criada para encontrar o id dentro da lista
-  res.json(livros[index]); //devolve o array inteiro após a alteração
-})
-
-
-app.put('/livros/:id',(req, res) => {
-  let index = buscarLivro(req.params.id); //utiliza a funcao criada para encontrar o id dentro da lista
-  livros[index].titulo = req.body.titulo; //altera o titulo do livro com o index encontrado
-  res.json(livros); //devolve o array inteiro após a alteração
-})
-
-app.delete('/livros/:id',(req, res) => {
-  let {id} = req.params //atribuição via desestruturação
-  let index = buscarLivro(id); //utiliza a funcao criada para encontrar o id dentro da lista
-  livros.splice(index, 1) //método para apagar com os parametros posição e quantidade de dados (1)
-  res.send(`Livro ${id} removido com sucesso`); 
-})
-
-function buscarLivro(id){
-  return livros.findIndex(livro => livro.id == id)
-}
+routes(app); //direcionamento para as rotas
 
 export default app
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -8,5 +8,6 @@ router
   .get("/livros/:id", LivroController.listarLivroId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
+  .delete("/livros/:id", LivroController.excluirLivro)
 
 export default router;
